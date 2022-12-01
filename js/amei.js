b9db1f208bbf39fe78c440d6e94dccfe11b41177
claudinei <claudineigaiteiro@gmail.com>
2022-12-01 14:50:18 -0300

    first commit

---
+++ js/amei.js
@@ -0,0 +1,95 @@
+const dandoLike1 = document.querySelector('.curtida1');
+
+dandoLike1.addEventListener('click', function (){
+    dandoLike1.classList.toggle('curtido1');
+});
+
+const dandoLike2 = document.querySelector('.curtida2');
+
+dandoLike2.addEventListener('click', function (){
+    dandoLike2.classList.toggle('curtido2');
+});
+
+const dandoLike3 = document.querySelector('.curtida3');
+
+dandoLike3.addEventListener('click', function (){
+    dandoLike3.classList.toggle('curtido3');
+});
+
+const dandoLike4 = document.querySelector('.curtida4');
+
+dandoLike4.addEventListener('click', function (){
+    dandoLike4.classList.toggle('curtido4');
+});
+
+const dandoLike5 = document.querySelector('.curtida5');
+
+dandoLike2.addEventListener('click', function (){
+    dandoLike2.classList.toggle('curtido5');
+});
+
+const dandoLike6 = document.querySelector('.curtida6');
+
+dandoLike6.addEventListener('click', function (){
+    dandoLike6.classList.toggle('curtido6');
+});
+
+const dandoLike7 = document.querySelector('.curtida7');
+
+dandoLike7.addEventListener('click', function (){
+    dandoLike7.classList.toggle('curtido7');
+});
+
+const dandoLike8 = document.querySelector('.curtida8');
+
+dandoLike8.addEventListener('click', function (){
+    dandoLike8.classList.toggle('curtido8');
+});
+
+const dandoLike9 = document.querySelector('.curtida9');
+
+dandoLike9.addEventListener('click', function (){
+    dandoLike9.classList.toggle('curtido9');
+});
+
+const dandoLike10 = document.querySelector('.curtida10');
+
+dandoLike10.addEventListener('click', function (){
+    dandoLike10.classList.toggle('curtido10');
+});
+
+const dandoLike11 = document.querySelector('.curtida11');
+
+dandoLike11.addEventListener('click', function (){
+    dandoLike11.classList.toggle('curtido11');
+});
+
+const dandoLike12 = document.querySelector('.curtida12');
+
+dandoLike12.addEventListener('click', function (){
+    dandoLike12.classList.toggle('curtido12');
+});
+
+const dandoLike13 = document.querySelector('.curtida13');
+
+dandoLike13.addEventListener('click', function (){
+    dandoLike13.classList.toggle('curtido13');
+});
+
+const dandoLike14 = document.querySelector('.curtida14');
+
+dandoLike14.addEventListener('click', function (){
+    dandoLike14.classList.toggle('curtido14');
+});
+
+const dandoLike15 = document.querySelector('.curtida15');
+
+dandoLike15.addEventListener('click', function (){
+    dandoLike15.classList.toggle('curtido15');
+});
+
+const dandoLike16 = document.querySelector('.curtida16');
+
+dandoLike16.addEventListener('click', function (){
+    dandoLike16.classList.toggle('curtido16');
+});
---
+++ js/contadorLike.js
@@ -0,0 +1,9 @@
+var data = 0;
+
+
+document.getElementById("counting").innerText = data;
+
+function increment(){
+    data = data + 1;
+    document.getElementById("counting").innerText = data;
+}
